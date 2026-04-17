9705475b2f869100c5a545cbf2b98bd8c47d7924
live-by-unix <abhayscontact@gmail.com>
2026-04-16 17:12:44 -0700

    Update homebrew-pong-linux.rb

--- Formula/homebrew-pong-linux.rb
+++ Formula/homebrew-pong-linux.rb
@@ -1,18 +1,18 @@
 class HomebrewPongLinux < Formula
-  desc "A simple Pong game written in GDScript (Linux Build) "
-  homepage "https://github.com/TheLinuxGroup/homebrew-pong-linux"
-  url "https://github.com/TheLinuxGroup/homebrew-pong-linux/releases/download/v0.9.0/pong-linux.tar.gz"
+  desc "A simple Pong game written in GDScript"
+  homepage "https://github.com/live-by-unix/homebrew-pong-linux"
+  url "https://github.com/live-by-unix/homebrew-pong-linux/releases/download/v0.9.0/pong-linux.tar.gz"
   sha256 "081d482db6c2861e78b57ee99c657bf7eebb486c6e048f3b1ce682f0006c629d"
   license "MIT"
 
   def install
+    # This handles both potential naming conventions from your Godot export
     if File.exist?("pong.x86_64")
       bin.install "pong.x86_64" => "pong"
     elsif File.exist?("homebrew-pong-linux.x86_64")
       bin.install "homebrew-pong-linux.x86_64" => "pong"
     end
 
-    # Checking for the data pack
     if File.exist?("pong.pck")
       bin.install "pong.pck"
     elsif File.exist?("homebrew-pong-linux.pck")
@@ -22,14 +22,16 @@ class HomebrewPongLinux < Formula
 
   def caveats
     <<~EOS
-      To start the game, simply type:
-        pong
+      Pong has been installed! 
+      To play, simply type 'pong' in your terminal.
       
-      If you get a Vulkan or Graphics error, ensure your drivers are up to date.
+      Note: If the game fails to launch, ensure you have Vulkan or OpenGL 
+      drivers installed for your Linux distribution.
     EOS
   end
 
   test do
+    # This checks if the 'pong' command was successfully linked to the bin
     assert_predicate bin/"pong", :executable?
   end
 end
