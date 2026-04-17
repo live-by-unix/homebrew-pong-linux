2e4a948c30ea44ee45117dbbcaf3a21bb0b2d1ad
live-by-unix <abhayscontact@gmail.com>
2026-04-16 17:03:18 -0700

    Update pong.rb

--- Formula/pong.rb
+++ Formula/pong.rb
@@ -1,29 +1,31 @@
-class Pong < Formula
-  desc "A simple Pong game written in GDScript"
-  homepage "https://github.com/TheLinuxGroup/Pong"
-  url "https://github.com/TheLinuxGroup/Pong/releases/download/v0.9.0/pong-linux.tar.gz"
+class HomebrewPongLinux < Formula
+  desc "A simple Pong game written in GDScript (Linux Build) "
+  homepage "https://github.com/TheLinuxGroup/homebrew-pong-linux"
+  url "https://github.com/TheLinuxGroup/homebrew-pong-linux/releases/download/v0.9.0/pong-linux.tar.gz"
   sha256 "081d482db6c2861e78b57ee99c657bf7eebb486c6e048f3b1ce682f0006c629d"
-  license "MIT" 
+  license "MIT"
 
   def install
     if File.exist?("pong.x86_64")
       bin.install "pong.x86_64" => "pong"
-    elsif File.exist?("Pong.x86_64")
-      bin.install "Pong.x86_64" => "pong"
+    elsif File.exist?("homebrew-pong-linux.x86_64")
+      bin.install "homebrew-pong-linux.x86_64" => "pong"
     end
 
- 
+    # Checking for the data pack
     if File.exist?("pong.pck")
       bin.install "pong.pck"
-    elsif File.exist?("Pong.pck")
-      bin.install "Pong.pck"
+    elsif File.exist?("homebrew-pong-linux.pck")
+      bin.install "homebrew-pong-linux.pck"
     end
   end
 
   def caveats
     <<~EOS
-      If the game fails to start, make sure you have 'libvulkan1' or 'libgl1' 
-      installed on your system via your distribution's package manager.
+      To start the game, simply type:
+        pong
+      
+      If you get a Vulkan or Graphics error, ensure your drivers are up to date.
     EOS
   end
 
