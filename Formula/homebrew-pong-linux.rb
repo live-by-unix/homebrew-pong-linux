eefbfd0a74d26c8269a79c9d5c2184b4d3aaf80c
live-by-unix <abhayscontact@gmail.com>
2026-04-16 17:17:56 -0700

    Update homebrew-pong-linux.rb

--- Formula/homebrew-pong-linux.rb
+++ Formula/homebrew-pong-linux.rb
@@ -1,37 +1,35 @@
 class HomebrewPongLinux < Formula
-  desc "A simple Pong game written in GDScript"
-  homepage "https://github.com/live-by-unix/homebrew-pong-linux"
-  url "https://github.com/live-by-unix/homebrew-pong-linux/releases/download/v0.9.0/pong-linux.tar.gz"
+  desc "Pong game in GDScript (Source: TheLinuxGroup)"
+  homepage "https://github.com/TheLinuxGroup/Pong"
+  
+  # Pointing to the actual source of the binaries
+  url "https://github.com/TheLinuxGroup/Pong/releases/download/v0.9.0/pong-linux.tar.gz"
   sha256 "081d482db6c2861e78b57ee99c657bf7eebb486c6e048f3b1ce682f0006c629d"
   license "MIT"
 
   def install
-    # This handles both potential naming conventions from your Godot export
+    # Look for the files inside the tar.gz from TheLinuxGroup
     if File.exist?("pong.x86_64")
       bin.install "pong.x86_64" => "pong"
-    elsif File.exist?("homebrew-pong-linux.x86_64")
-      bin.install "homebrew-pong-linux.x86_64" => "pong"
+    elsif File.exist?("Pong.x86_64")
+      bin.install "Pong.x86_64" => "pong"
     end
 
     if File.exist?("pong.pck")
       bin.install "pong.pck"
-    elsif File.exist?("homebrew-pong-linux.pck")
-      bin.install "homebrew-pong-linux.pck"
+    elsif File.exist?("Pong.pck")
+      bin.install "Pong.pck"
     end
   end
 
   def caveats
     <<~EOS
-      Pong has been installed! 
-      To play, simply type 'pong' in your terminal.
-      
-      Note: If the game fails to launch, ensure you have Vulkan or OpenGL 
-      drivers installed for your Linux distribution.
+      Successfully installed from TheLinuxGroup/Pong.
+      Type 'pong' to launch the game.
     EOS
   end
 
   test do
-    # This checks if the 'pong' command was successfully linked to the bin
     assert_predicate bin/"pong", :executable?
   end
 end
